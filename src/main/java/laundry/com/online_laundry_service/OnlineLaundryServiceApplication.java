18b48bf1b9e8ec9f139d56a5037fc774eed709ee
Mohammed Al Sileam <alsalemm666@gmail.com>
2025-10-09 17:12:30 +0300

    Initial project setup and code

---
+++ src/main/java/laundry/com/online_laundry_service/OnlineLaundryServiceApplication.java
@@ -0,0 +1,12 @@
+package laundry.com.online_laundry_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLaundryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLaundryServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/laundry/com/online_laundry_service/OnlineLaundryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package laundry.com.online_laundry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLaundryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
