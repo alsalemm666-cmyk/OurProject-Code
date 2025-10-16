623c8a27aedd968b6b65a58ebc5b750839613863
Mohammed Al Sileam <alsalemm666@gmail.com>
2025-10-16 18:47:05 +0300

    user service updet

--- src/main/java/laundry/com/online_laundry_service/Services/Userservice.java
+++ src/main/java/laundry/com/online_laundry_service/Services/Userservice.java
@@ -1,4 +1,6 @@
 package laundry.com.online_laundry_service.Services;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,29 +14,29 @@ public class Userservice {
 
     // إنشاء مستخدم جديد
     public User createUser(User user) {
-        return userRepository.save(user);
+        return userrepository.save(user);
     }
 
     // عرض جميع المستخدمين
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return userrepository.findAll();
     }
 
     // جلب مستخدم حسب الـ ID
     public Optional<User> getUserById(Long id) {
-        return userRepository.findById(id);
+        return userrepository.findById(id);
     }
 
     // تحديث بيانات المستخدم
     public User updateUser(Long id, User updatedUser) {
-        Optional<User> existingUser = userRepository.findById(id);
+        Optional<User> existingUser = userrepository.findById(id);
         if (existingUser.isPresent()) {
             User user = existingUser.get();
             user.setName(updatedUser.getName());
             user.setEmail(updatedUser.getEmail());
             user.setPassword(updatedUser.getPassword());
             user.setRole(updatedUser.getRole());
-            return userRepository.save(user);
+            return userrepository.save(user);
         } else {
             return null; // أو ممكن ترمين استثناء لو تبغين
         }
@@ -42,7 +44,7 @@ public class Userservice {
 
     // حذف مستخدم
     public void deleteUser(Long id) {
-        userRepository.deleteById(id);
+        userrepository.deleteById(id);
     }
 
 }
